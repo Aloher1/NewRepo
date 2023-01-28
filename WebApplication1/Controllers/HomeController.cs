303a1b2b61669b25ecf3822d0b4cb317eab7bcbf
user <user@PC20>
2023-01-28 19:11:15 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using System.Security.Permissions;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<product> list = new List<product>();
+            product products = new product();
+            products.id = 1;
+            products.name = "1";
+            products.price = 100;
+            list.Add(products);
+            products.id = 2;
+            products.name = "2";
+            products.price = 200;
+
+            list.Add(products);
+
+            ViewBag.product = list;
+            return View();
+        }
+     
+        /*
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
